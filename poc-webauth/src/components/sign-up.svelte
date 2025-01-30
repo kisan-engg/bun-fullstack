bd040f6e655f192100a9ee24e56326251b813715
mr.rajeevkrmail@gnail.com <mr.rajeevkrmail@gnail.com>
2025-01-30 13:25:55 +0000

    updated sign up

--- poc-webauth/src/components/sign-up.svelte
+++ poc-webauth/src/components/sign-up.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
+    import { signUp } from "../lib/auth-client";
+
     const { alreadyRegistered } = $props();
+
+    let signUpForm = $state({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const userSignUp = async (event: { preventDefault: () => void; }) => {
+        if(event.preventDefault)
+            event.preventDefault();
+        const { data, error } = await signUp.email({...signUpForm});
+        console.log("data,", data);
+        console.log("error, ", error);
+    }
 </script>
-<form class="max-w-md mx-auto">
+<form class="max-w-md mx-auto" onsubmit={userSignUp}>
     <div class="relative z-0 w-full mb-5 group">
         <input
             type="email"
             name="user_email"
             id="user_email"
+            bind:value={signUpForm.email}
             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
@@ -22,8 +39,10 @@
             type="password"
             name="user_password"
             id="user_password"
+            bind:value={signUpForm.password}
             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
+            minlength="8"
             required
         />
         <label
@@ -32,7 +51,7 @@
             >Password</label
         >
     </div>
-    <div class="relative z-0 w-full mb-5 group">
+    <!-- <div class="relative z-0 w-full mb-5 group">
         <input
             type="password"
             name="user_repeat_password"
@@ -46,40 +65,25 @@
             class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >Confirm password</label
         >
+    </div> -->
+    <div class="relative z-0 w-full mb-5 group">
+        <input
+            type="text"
+            name="user_first_name"
+            id="user_first_name"
+            bind:value={signUpForm.name}
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            minlength="3"
+            required
+        />
+        <label
+            for="user_first_name"
+            class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >Name</label
+        >
     </div>
-    <div class="grid md:grid-cols-2 md:gap-6">
-        <div class="relative z-0 w-full mb-5 group">
-            <input
-                type="text"
-                name="user_first_name"
-                id="user_first_name"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required
-            />
-            <label
-                for="user_first_name"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >First name</label
-            >
-        </div>
-        <div class="relative z-0 w-full mb-5 group">
-            <input
-                type="text"
-                name="user_last_name"
-                id="user_last_name"
-                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                placeholder=" "
-                required
-            />
-            <label
-                for="user_last_name"
-                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >Last name</label
-            >
-        </div>
-    </div>
-    <div class="grid md:grid-cols-2 md:gap-6">
+    <!-- <div class="grid md:grid-cols-2 md:gap-6">
         <div class="relative z-0 w-full mb-5 group">
             <input
                 type="tel"
@@ -111,7 +115,7 @@
                 >Company (Ex. Google)</label
             >
         </div>
-    </div>
+    </div> -->
     <div class="grid grid-cols-2 gap-8">
         <div class="relative z-0 w-full mb-5 group ms-2">
             <button 
@@ -124,7 +128,7 @@
         </div>
         <div class="relative z-0 w-full mb-5 group text-right">
             <button 
-                type="button" 
+                type="submit" 
                 class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
                 Register
             </button>
